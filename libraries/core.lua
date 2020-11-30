4f3b7d16b10d7a0b4198794626939cdc61128c33
Kammon <jjc15433@gmail.com>
2020-11-29 20:18:35 -0500

    Update core.lua

--- libraries/core.lua
+++ libraries/core.lua
@@ -1,1 +1,16 @@
 --things like join, split, selectFromTable, etc will live here
+
+--table utility functions
+function table:print()
+    assert(type(self)=="table")
+    print("\nkey\t\t\tvalue")
+    print("---\t\t\t-----")
+    for k,v in next, self do
+        print(k,"\t\t\t",v)
+        --os.sleep(1) -- because cc terminals have garbage height and no scroll capability
+        if type(v)=="table" then
+            table.print(v)
+        end
+    end
+    print("")
+end

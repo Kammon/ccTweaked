db628adfd1c6d365076554430ded84e992e8b2c8
Josh Cole <jjc15433@gmail.com>
2020-12-13 10:50:23 -0500

    pastebin startup script

--- pastebin/startup.lua
+++ pastebin/startup.lua
@@ -1,1 +1,36 @@
-bootstrapPath=""
+--[[
+	
+	
+	Basic ccTweaked Computer Startup Script
+
+	Startup (a static file hosted on Pastebin) does the following:
+		-downloads a file from a given path as "bootstrap"
+		-runs that file
+	
+	To-Do:
+		-bootstrap will:
+			-download repo paths file (github)
+			-run download code on repo paths
+			-erase basic pastebin startup (in favor of more robust startup dir from repo)
+			-erase repo paths file
+			-erase bootstrap
+
+
+--]]
+
+
+bootstrapPath="https://raw.githubusercontent.com/Kammon/ccTweaked/main/pastebin/bootstrap.lua"
+
+local resp = http.get(bootstrapPath)
+if resp.getResponseCode() == 200 and tonumber(resp.getResponseHeaders()["content-length"]) > 0 then
+	local file = fs.open("bootstrap","w")
+	local line = resp.readLine(true)
+	while line ~= nil do
+		file.write(line)
+		line=resp.readLine(true)
+	end
+	file.close()
+end
+resp.close()
+
+shell.run("bootstrap")

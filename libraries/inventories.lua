7ea22f66a28c41090ba99f154d3528e29be1bffd
Kammon <jjc15433@gmail.com>
2020-12-20 15:58:13 -0500

    Update inventories.lua

--- libraries/inventories.lua
+++ libraries/inventories.lua
@@ -1,15 +1,15 @@
 local fsu = require("/libraries/fsUtils") -- not entirely sure how I want these to work
 local core = require("/libraries/core")
-
-function getInventories()
-	return fsu.getContents("/libraries/datasets/inventories.lua")
+local inventories = {}
+function inventories.getInventories()
+	return fsu.getContents(core.bp.."data/inventory/inventories.lua")
 end
 
-function getInventoryTypes()
-	return fsu.getContents("/libraries/datasets/inventoryTypes.lua")
+function inventories.getInventoryTypes()
+	return fsu.getContents(core.bp.."data/inventoryTypes.lua")
 end
 
-function isInventory(slot)
+function inventories.isInventory(slot)
 	local slotItem, isInv = turtle.getItemDetail(slot), false
 	for k,v in pairs(getInventories()) do
 		if slotItem then
@@ -19,12 +19,17 @@ function isInventory(slot)
 	return isInv
 end
 
-function checkInventory(slot)
-	local slotInv, thisInv, currSlot = slot, "unknown", turtle.getSelectedSlot()
-	local invTypes = {}
+function inventories.checkInventory(slot)
+	local slotInv, thisInv, currSlot = slot, {type="unknown", slot=-1}, turtle.getSelectedSlot()
+	local invTypes, kvParts = {}, {}
 	for k,v in pairs(getInventoryTypes()) do
 		--working on improving fileRead and split to make k,v pairs
-		core
+		kvParts = core.split(v,"=",nil,false)
+		if #kvParts == 2 then
+			invTypes[core.trim(kvParts[1])] = core.trim(kvParts[2])
+		else
+			print("ERROR: Invalid Key/Value pair at "..v)
+		end
 	end
 	while turtle.detectUp() do turtle.digUp(); os.sleep(1) end
 	turtle.select(slotInv)
@@ -33,12 +38,14 @@ function checkInventory(slot)
 		turtle.suckUp()
 		local invType = turtle.getItemDetail()
 		if invType then
-			if invType.name == v then thisInv = k end -- NOTE: This won't work atm. Current fileRead only supports numbered indexes
+			if invType.name == v then thisInv.type = k; thisInv.slot = slotInv end -- NOTE: This won't work atm. Current fileRead only supports numbered indexes
 			turtle.dropUp()
 		else
-			thisInv = "empty"
+			thisInv.type = "empty"; thisInv.slot = slotInv
 		end
 	end
 	turtle.digUp() -- NOTE: not entirely sure the behavior I want here. Do I leave the inventory up there and let the caller decide what to do, or do I note the invType and slot and relay that to the caller instead?
 	return thisInv
 end
+
+return inventories

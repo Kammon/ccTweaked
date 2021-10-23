0116ab38a48c5a2610d3aa5b073fc29c8be1b3e0
Josh Cole <jjc15433@gmail.com>
2021-10-22 23:11:33 -0400

    lazy tree farm prototype

--- luaTesting.lua
+++ luaTesting.lua
@@ -1,6 +1,28 @@
 inv = require("/repos/Kammon/ccTweaked/libraries/inventories")
 turtle = require("/rom/apis/turtle/turtle")
 
-for i= 1, inv.core.SLOT_COUNT do
-	turtle.getItemDetails(i)
+while true do
+	entity = turtle.inspect();
+	while entity and entity.name ~= "minecraft:oak_log" do
+		os.sleep(10);
+		entity = turtle.inspect();
+	end
+	i, continue = 1, true;
+	while continue do
+		turtle.dig()
+		turtle.forward()
+		blockAbove = turtle.inspectUp()
+		while blockAbove and blockAbove.name ~= "minecraft:glass_pane" do
+			turtle.digUp()
+			turtle.up()
+			blockAbove = turtle.inspectUp()
+		end
+		while not turtle.detectDown() do turtle.down() end
+		entity = turtle.inspect()
+		if entity and entity.name ~= "minecraft:oak_log" then continue = false else i=i+1 end
+	end
+	for i=i,1,-1 do
+		turtle.back()
+		turtle.place() -- this is lazily assuming the current slot has saplings, sue me, it's test code
+	end
 end

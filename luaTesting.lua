d0396fe21cc1afb5ade49e8f49bfd1be831e5c5f
Josh Cole <jjc15433@gmail.com>
2021-10-22 23:16:59 -0400

    lazy tree farm prototype

--- luaTesting.lua
+++ luaTesting.lua
@@ -1,25 +1,25 @@
 inv = require("/repos/Kammon/ccTweaked/libraries/inventories")
-turtle = require("/rom/apis/turtle/turtle")
+--turtle = require("/rom/apis/turtle/turtle")
 
 while true do
-	entity = turtle.inspect();
-	while entity and entity.name ~= "minecraft:oak_log" do
+	present, entity = turtle.inspect();
+	while present and entity.name ~= "minecraft:oak_log" do
 		os.sleep(10);
-		entity = turtle.inspect();
+		present, entity = turtle.inspect();
 	end
 	i, continue = 1, true;
 	while continue do
 		turtle.dig()
 		turtle.forward()
-		blockAbove = turtle.inspectUp()
-		while blockAbove and blockAbove.name ~= "minecraft:glass_pane" do
+		present, blockAbove = turtle.inspectUp()
+		while present and blockAbove.name ~= "minecraft:glass_pane" do
 			turtle.digUp()
 			turtle.up()
-			blockAbove = turtle.inspectUp()
+			present, blockAbove = turtle.inspectUp()
 		end
 		while not turtle.detectDown() do turtle.down() end
-		entity = turtle.inspect()
-		if entity and entity.name ~= "minecraft:oak_log" then continue = false else i=i+1 end
+		present, entity = turtle.inspect()
+		if present and entity.name ~= "minecraft:oak_log" then continue = false else i=i+1 end
 	end
 	for i=i,1,-1 do
 		turtle.back()

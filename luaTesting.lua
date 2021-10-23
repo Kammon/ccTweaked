e22e474bb67417a1cd97474567a032702f424edd
Josh Cole <jjc15433@gmail.com>
2021-10-22 23:54:18 -0400

    lazy tree farm prototype

--- luaTesting.lua
+++ luaTesting.lua
@@ -1,6 +1,8 @@
 inv = require("/repos/Kammon/ccTweaked/libraries/inventories")
 --turtle = require("/rom/apis/turtle/turtle")
 
+--[[
+-- front to back version (keep cutting down from start until there isn't a grown tree)
 while true do
 	present, entity = turtle.inspect();
 	while present and entity.name ~= "minecraft:oak_log" do
@@ -25,4 +27,30 @@ while true do
 		turtle.back()
 		turtle.place() -- this is lazily assuming the current slot has saplings, sue me, it's test code
 	end
+end
+--]]
+
+
+-- Row checking version (go down length of farm and inspect each tree)
+while true do
+	i, continue, present, entity = 1, true, turtle.inspect();
+	while present and entity.name == "minecraft:oak_log" or entity.name == "minecraft:oak_sapling" do
+		i=i+1;
+		if entity.name == "minecraft:oak_log" then
+			turtle.dig();
+			present, blockAbove = turtle.inspectUp()
+			while present && blockAbove.name ~= "minecraft:glass_pane" do turtle.digUp(); turtle.up(); end
+			while not turtle.detectDown() do turtle.down(); end
+			turtle.back();
+			turtle.place(); -- again assuming saplings in current slot
+		end
+		turtle.turnLeft();
+		turtle.forward();
+		turtle.turnRight();
+		present, entity = turtle.inspect();
+	end
+	turtle.turnRight();
+	for i=i,1,-1 do	turtle.forward() end
+	turtle.turnLeft();
+	os.sleep(300);
 end

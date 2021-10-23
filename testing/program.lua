bdc52731d490ae6ce3f68064912d5bcb56f7bb7f
Josh Cole <jjc15433@gmail.com>
2021-10-23 19:06:16 -0400

    arg testing

---
+++ testing/program.lua
@@ -0,0 +1,8 @@
+core = require("/repos/Kammon/ccTweaked/libraries/core")
+fsUtils = require("/repos/Kammon/ccTweaked/libraries/fsUtils")
+inv = require("/repos/Kammon/ccTweaked/libraries/inventories")
+fuel = require("/repos/Kammon/ccTweaked/testing/module3")
+
+function testArgs(args)
+	for i=1,#args do print(args[i]) end
+end
